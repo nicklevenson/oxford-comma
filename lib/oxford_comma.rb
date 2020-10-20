5d14a76f6f2652111d55d8e401052c97b90e4ed2
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-20 14:47:52 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size > 2 
+        lastItem = array[-1];
+        array.pop()
+        return array.join(", ") + ", and " + lastItem
+    elsif array.size == 2
+        return "#{array[0]} and #{array[1]}"
+    else 
+        return array[0];
+    end
 end
